581d3b67f3a47c0702e6394f60e07c8fcb5213fc
Elencrissy <cristina.furgala@gmail.com>
2021-11-30 17:00:34 +0100

    DailyActivities

--- src/components/DashboardComponents/DailyActivities/index.jsx
+++ src/components/DashboardComponents/DailyActivities/index.jsx
@@ -1,1 +1,18 @@
 import styled from "styled-components";
+import {
+    ResponsiveContainer,
+    AreaChart,
+    XAxis,
+    YAxis,
+    Area,
+    Tooltip,
+    CartesianGrid
+} from "recharts";
+
+function DailyActivities(){
+    return(
+        null
+    )
+}
+
+export default DailyActivities
