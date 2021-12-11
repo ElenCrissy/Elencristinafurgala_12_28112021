ab5b23f6d4157384abab023c9679a95031accfb1
Elencrissy <cristina.furgala@gmail.com>
2021-12-11 23:36:08 +0100

    Kpi ok

--- src/components/DashboardComponents/Kpi/kpiInfos.jsx
+++ src/components/DashboardComponents/Kpi/kpiInfos.jsx
@@ -6,12 +6,22 @@ import lipids from "../../../assets/cheeseburger.svg";
 
 
 const KpiContainer = styled.div`
-  width: 180px;
-  height: 80px;
+  width: 160px;
+  height: 70px;
   background-color: #FBFBFB;
   border-radius: 5px;
+  padding: 10px;
   margin-bottom: 20px;
   display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  img{
+    width: 17px;
+    height: 17px;
+    padding: 10px;
+    border-radius: 5px;
+    margin-left: 10px;
+  }
 `
 const KpiWrapper = styled.div`
   display: flex;
@@ -23,18 +33,23 @@ const KpiWrapper = styled.div`
   p:first-child{
     font-weight: bold;
   }
+  p:last-child{
+    color: grey;
+    font-size: 12px;
+    margin-top: 3px;
+  }
 `
 
 const KpiInfos = (props) => {
-
+    console.log(props.kpiName)
 
 
     return(
         <KpiContainer>
-            {props.kpiName === "calorieCount" ? (<img src={calories} alt=""/>) : null}
-            {props.kpiName === "proteinCount" ? (<img src={proteins} alt=""/>) : null}
-            {props.kpiName === "carbohydrateCount" ? (<img src={carbohydrates} alt=""/>) : null}
-            {props.kpiName === "lipidCount" ? (<img src={lipids} alt=""/>) : null}
+            {props.kpiName === "calorieCount" ? (<img src={calories} alt="" style={{background: "rgb(255, 0, 0, 0.1)"}}/>) : null}
+            {props.kpiName === "proteinCount" ? (<img src={proteins} alt="" style={{background: "rgb(74, 184, 255, 0.1)"}}/>) : null}
+            {props.kpiName === "carbohydrateCount" ? (<img src={carbohydrates} alt="" style={{background: "rgb(249, 206, 35, 0.1)"}}/>) : null}
+            {props.kpiName === "lipidCount" ? (<img src={lipids} alt="" style={{background: "rgb(253, 81, 129, 0.1)"}}/>) : null}
 
             <KpiWrapper>
                 <p>{props.kpiValue}{props.kpiName === "calorieCount" ? "kCal" : "g"}</p>
