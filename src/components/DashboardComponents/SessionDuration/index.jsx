a50ed7bfc52d21c902c1d206f2ebe0076e6d180e
Elencrissy <cristina.furgala@gmail.com>
2021-12-14 00:09:26 +0100

    AreaChart sessions

--- src/components/DashboardComponents/SessionDuration/index.jsx
+++ src/components/DashboardComponents/SessionDuration/index.jsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 import {
     AreaChart,
-    defs,
-    linearGradient,
-    stop,
     XAxis,
     YAxis,
     CartesianGrid,
@@ -12,24 +9,30 @@ import {
 } from "recharts";
 
 const SessionDuration = (props) => {
+    const data = props.data.sessions
+    console.log(data)
     return(
-        <AreaChart width={100} height={100} data={props.data}>
-            <defs>
-                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                </linearGradient>
-            </defs>
-            <XAxis dataKey="name" />
+        <AreaChart width={300}
+                   height={300}
+                   data={data}
+                   margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                   radius={5}
+                   style={{background: "#FF0000"}}>
+            {/*<defs>*/}
+            {/*    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">*/}
+            {/*        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>*/}
+            {/*        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>*/}
+            {/*    </linearGradient>*/}
+            {/*    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">*/}
+            {/*        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>*/}
+            {/*        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>*/}
+            {/*    </linearGradient>*/}
+            {/*</defs>*/}
+            <XAxis dataKey="day" />
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            <Area type="monotone" dataKey="sessionLength" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
         </AreaChart>
     )
 }
